50ba2e1a20dd5431c9a3603462cbf493a5000ea6
luislowy <lowy.luisangel@gmail.com>
2020-08-22 18:28:20 -0500

    h

--- carspro/settings.py
+++ carspro/settings.py
@@ -135,7 +135,7 @@ REST_FRAMEWORK = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'car/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'carspro/static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS=[
 os.path.join(BASE_DIR,'static'),]
