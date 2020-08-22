37da938873fc61854d87de88e129b402c034a0e5
luislowy <lowy.luisangel@gmail.com>
2020-08-22 17:24:15 -0500

    tan

--- carsapp/forms.py
+++ carsapp/forms.py
@@ -1,3 +1,96 @@
 from django import forms
 from .models import *
 from django.contrib.auth.models import User
+
+
+class agregar_persona_form(forms.ModelForm):
+	class Meta:
+		model=Persona
+		fields='__all__'
+
+class agregar_carro_form(forms.ModelForm):
+	class Meta:
+		model=Carro
+		fields='__all__'
+
+class confirmar_user_form(forms.Form):
+	identificacion=forms.CharField(widget= forms.TextInput(attrs={'tipe': 'number','placeholder':'Identificacion'}))
+	respuesta=forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Respuesta'}))
+
+	def clean_respuesta(self):
+		
+		respuesta=self.cleaned_data['respuesta']
+		identificacion=self.cleaned_data['identificacion']
+		if Persona.objects.filter(identificacion=identificacion).exists():
+			ide=Persona.objects.get(identificacion=identificacion)
+			if ide.respuesta== respuesta:
+				pass
+			else:
+				raise forms.ValidationError('respuesta erronea')
+			if ide.usuario:
+				raise forms.ValidationError('ya tiene usuario creado')
+			else:
+				pass
+		else:
+			raise forms.ValidationError('no se encuentra registrado')
+
+
+class login_form(forms.Form):
+	
+	usuario = forms.CharField(widget= forms.TextInput(attrs={'class':'form-control','placeholder':'Nombre user'}))
+	clave = forms.CharField(widget= forms.PasswordInput(render_value=False, attrs={'class':'form-control', 'placeholder':'Contraseña'}))
+	
+
+class register_form(forms.Form):
+	username = forms.CharField(widget = forms.TextInput(attrs={'placeholder':'Nombre user'}))
+	email = forms.EmailField(widget = forms.TextInput(attrs={'placeholder':'Email'}))
+	password1 = forms.CharField(label = 'Password', widget=forms.PasswordInput(render_value=False, attrs={'placeholder':'Contraseña'}))
+	password2 = forms.CharField(label = 'Confirmar Password', widget=forms.PasswordInput(render_value=False, attrs={'placeholder':'Contraseña'}))
+	
+
+
+	def clean_username(self):
+		username=self.cleaned_data['username']
+		try:
+			u=User.objects.get(username=username)
+		except  User.DoesNotExist:
+			return username
+		raise forms.ValidationError('Nombre de user ya existe')
+
+	def clean_email(self):
+
+		email=self.cleaned_data['email']
+		try:
+			email=User.objects.get(email=email)
+		except User.DoesNotExist:
+			return email
+		raise forms.ValidationError('email ya existe')
+
+	def clean_password2(self):
+		password1=self.cleaned_data['password1']
+		password2=self.cleaned_data['password2']
+		if password1==password2:
+			pass
+		else:
+			raise forms.ValidationError('Password no coinciden')
+
+class contacto_form(forms.ModelForm):   # se utiliza forms.ModelForm cuando se quiere trabajar con el formulario del diseño
+	class Meta:                         #de los modelos de la ase de datos
+		model=Contacto
+		fields='__all__'
+	
+class dispositivos_form(forms.Form):
+	idalarma = forms.CharField(widget = forms.TextInput())
+
+class notificacion_form(forms.ModelForm):
+	class Meta:
+		model=Notificacion
+		fields='__all__'
+
+
+
+
+	
+
+
+
--- carsapp/urls.py
+++ carsapp/urls.py
@@ -1,10 +1,34 @@
 from django.urls import path
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView, TokenRefreshView,
-)
+from .views import *
+from django.contrib.auth.decorators import login_required
+from rest_framework_simplejwt.views import(TokenObtainPairView, TokenRefreshView,)
+from django.core.validators import URLValidator
+from django.core.exceptions import ValidationError
 
 urlpatterns=[
- path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
- path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+path('', vista_inicio, name='vista_inicio')
+path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+path('api/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+path('agregar_persona/', login_required (vista_agregar_persona), name= 'vista_agregar_persona'),
+path('agregar_carro/', vista_agregar_carro, name= 'vista_agregar_carro'),
+path('confirmar_user/', vista_confirmar_user, name='vista_confirmar_user'),
+path('mensaje/', vista_mensaje, name= 'vista_mensaje'),
+path('accounts/login/', vista_login, name='vista_login'),
+path('logout/', vista_logout, name='vista_logout'),
+path('register/<str:iden>/', vista_register, name='vista_register'),
+path('inicio/', vista_inicio, name='vista_inicio'),
+path('lista_persona/', vista_lista_persona, name='vista_lista_persona'),
+path('lista_carro/', vista_lista_carro, name='vista_lista_carro'),
+path('lista_token/', vista_lista_token, name='vista_lista_token'),
+path('contacto/', vista_contacto, name='vista_contacto'),
+path('eliminar_persona/<int:id_Persona>/', vista_eliminar_persona, name= 'vista_eliminar_persona'),
+path('eliminar_carro/<int:id_Carro>/', vista_eliminar_carro, name= 'vista_eliminar_carro'),
+path('notificacion/',  vista_notificacion, name='vista_notificacion'),
+path('inicio_superuser/',login_required (vista_inicio_superuser), name='inicio_superuser'),
+path('tokenRfirebase/', tokenRfirebase, name='tokenRfirebase'),
+path('resibir_info_controlador/',resibir_info_controlador, name='resibir_info_controlador'),
+path('liberar/token/', liberar_token, name='liberar_token'),
+path('solodata/', solodata, name='solodata')
+
 
 ]
