4977790bac44731fc79fee9f09a4b3aa90a614df
luislowy <lowy.luisangel@gmail.com>
2020-08-22 20:12:38 -0500

    dsf

--- carspro/settings.py
+++ carspro/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -140,10 +141,10 @@ MIDDLEWARE = [
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'carspro/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-'''STATICFILES_DIRS=[
-os.path.join(BASE_DIR,'static'),]'''
+STATICFILES_DIRS=[
+os.path.join(BASE_DIR,'static'),]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
