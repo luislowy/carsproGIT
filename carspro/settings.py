53078963c9a1296820a4592e6feed9b4365a0171
luislowy <lowy.luisangel@gmail.com>
2020-08-22 19:22:55 -0500

    dfd

--- carspro/settings.py
+++ carspro/settings.py
@@ -131,14 +131,19 @@ REST_FRAMEWORK = {
     )
     
 }
+MIDDLEWARE = [
+    
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'carspro/static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS=[
 os.path.join(BASE_DIR,'static'),]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 MEDIA_ROOT = 'media'
