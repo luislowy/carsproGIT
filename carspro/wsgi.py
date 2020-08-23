9ebb43e60827e7d3c2edc877b20bfc2105331bf4
luislowy <lowy.luisangel@gmail.com>
2020-08-22 19:34:25 -0500

    cvb

--- carspro/wsgi.py
+++ carspro/wsgi.py
@@ -10,8 +10,8 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.Django import DjangoWhiteNoise
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'carspro.settings')
 
-application = DjangoWhiteNoise(application)
+
 application = get_wsgi_application()
